c72618e4ad93129ae616b2bf5beb22b11f16e176
Igor Buzykin <lexemz@gmail.com>
2022-08-04 20:59:36 +0300

    Part 2 - Selection Search

---
+++ Grokking-Algorithms.playground/Pages/Selection Search.xcplaygroundpage/Contents.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+// Найти наименьший элемент массива O(N)
+func findSmallest(array: [Int]) -> Int {
+    var smallest = array[0]
+    var smallestIndex = 0
+    
+    for i in 0 ..< array.count {
+        if array[i] < smallest {
+            smallest = array[i]
+            smallestIndex = i
+        }
+    }
+    return smallestIndex
+}
+
+// Проходим по массиву и создаем новый O(N)
+func selectionSort(array: [Int]) -> [Int] {
+    guard array.count > 1 else { return array }
+    var array = array
+    var newArray: [Int] = []
+    
+    for _ in 0 ..< array.count {
+        let smallestIndex = findSmallest(array: array)
+        newArray.append(array.remove(at: smallestIndex))
+    }
+    return newArray
+}
+
+// O(N^2)
+selectionSort(array: [5, 6, 2, 1, 9, 2, 2])
+selectionSort(array: [1, 1, 1, 1])
+selectionSort(array: [2])
